188a49f1085f0c517c0c6d2b1a90718b2e84f7a5
Garett-Lubecki <garett934@gmail.com>
2023-07-27 18:09:33 -0400

    work on popover for breakdown

--- src/components/Reviews/RatingBreakDown.jsx
+++ src/components/Reviews/RatingBreakDown.jsx
@@ -1,10 +1,27 @@
 import './RatingBreakDown.css'
 import './Summary.css'
-import { useContext } from 'react'
+import { useContext, useState} from 'react'
 import ReviewsContext from '../../context/ReviewsContext'
+import RatingBreakDown_Popover from './RatingBreakDown_Popover'
+
+
 
 const RatingBreakDown = () => {
     const { breakDown } = useContext(ReviewsContext);
+    const [isShown, setShown] = useState({
+        column1: false,
+        column2: false,
+        column3: false,
+        column4: false,
+        column5: false,
+      });
+
+    const handleMouse = (columnId) => {
+        setShown((prevState) => ({
+            ...prevState,
+            [columnId]: !prevState[columnId]
+          }))
+    }
 
     const ratingColumns = [
         { id: 'column1', label: '5 star', percentage: breakDown.fiveStarPercentage, count: breakDown.fiveStar },
@@ -14,18 +31,21 @@ const RatingBreakDown = () => {
         { id: 'column5', label: '1 star', percentage: breakDown.oneStarPercentage, count: breakDown.oneStar },
     ];
 
+
     return (
         <div className='summary_RatingBreakDown'>
             <div className="breakdown_title">Ratings Breakdown</div>
+
             <div className="table">
                 {ratingColumns.map((column, index) => (
-                    <div className='column' key={column.id}>
+                    <div className='column' key={column.id} onMouseEnter={() => handleMouse(column.id)}  onMouseLeave={ () => handleMouse(column.id)}>
+                        {isShown[column.id] ? <RatingBreakDown_Popover label = {column.label}/> : <></>}
                         <div className="side">
                             <div>{column.label}</div>
                         </div>
                         <div className="middle">
                             <div className="bar-container">
-                                <div className={`bar-${index}`} style={{ width: `${column.percentage}%` }}></div>
+                                <div className={`bar-${index}`} style={{ width: `${column.percentage}%` }} ></div>
                             </div>
                         </div>
                         <div className="side-right">
